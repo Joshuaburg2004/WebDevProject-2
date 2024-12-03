bd1bbbaa3ef90f796feab891a99bbe7620e1879a
Ber-kan <bhgorur@gmail.com>
2024-12-03 10:11:47 +0100

    idee rooster

--- userattendance/src/App.js
+++ userattendance/src/App.js
@@ -4,8 +4,13 @@ function App() {
   return (
     
 <div>
-  <h1></h1>
-      {}
+  <h1>Attendance</h1>
+  <h2>Monday</h2> <h3> on site or online</h3>
+  <h2>Tuesday</h2>
+  <h2>Wednesday</h2>
+  <h2>Thursday</h2>
+  <h2>Friday</h2>
+
       
 </div>
   );
