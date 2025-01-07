b039389a10a81effa5f0bb97ce94da7c3ac603dc
Joshuaburg2004 <joshuaburg2004@hotmail.nl>
2025-01-07 11:07:36 +0100

    Added comment

--- frontend/src/home.tsx
+++ frontend/src/home.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 
+// Extend when necessary for another case in the render for HomePage
 export type HomeView = 
     'home' |
     'registration' |
@@ -15,11 +16,30 @@ export interface User{
 
 export interface HomeState{
     view: HomeView
-    currUser: User
+    currUser: User | undefined
     loggedIn: boolean
     setView: (view: HomeView) => (state: HomeState) => HomeState
-
+    setCurrUser: (user: User) => (state: HomeState) => HomeState
+    emptyCurrUser: (state: HomeState) => HomeState
 }
+
+export const initHomeState = (): HomeState => ({
+    view: "home",
+    currUser: undefined,
+    loggedIn: false,
+    setView: (view: HomeView) => (state: HomeState) => ({
+        ...state,
+        view: view
+    }),
+    setCurrUser: (user: User) => (state: HomeState) => ({
+        ...state,
+        user: user
+    }),
+    emptyCurrUser: (state: HomeState) => ({
+        ...state,
+        user: undefined
+    })
+})
 
 export class HomePage extends React.Component<{}, HomeState> {
     
