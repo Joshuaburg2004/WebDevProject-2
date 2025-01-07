36360b55511e4e533e666f3542890481c7788f5d
Joshuaburg2004 <joshuaburg2004@hotmail.nl>
2025-01-07 11:06:57 +0100

    Added initHomeState and HomeState, also added a fourth value to HomeView

--- frontend/src/home.tsx
+++ frontend/src/home.tsx
@@ -1,11 +1,26 @@
+import React from "react"
+
 export type HomeView = 
     'home' |
     'registration' |
-    'login'
+    'login' |
+    'userattendance'
 
 export interface User{
     id: number
     username: string
     email: string
     password: string
+}
+
+export interface HomeState{
+    view: HomeView
+    currUser: User
+    loggedIn: boolean
+    setView: (view: HomeView) => (state: HomeState) => HomeState
+
+}
+
+export class HomePage extends React.Component<{}, HomeState> {
+    
 }
