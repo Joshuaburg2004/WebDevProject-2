c354500a77bde747709ca1ef306205e31765ae41
Sami Jerrari <SamiJerrari547@gmail.com>
2025-01-11 14:31:44 +0100

    non working app for frontendfolder, working version app.js for my own folder  . (i have to convert it)

---
+++ frontend/src/eventsreact.tsx
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+ 
+export const Eventsreact = (): JSX.Element => {
+    const [newEvent, setNewEvent] = useState('');
+  return (
+    <div>
+    <h1>Events</h1>
+    <h2>Add event</h2>
+
+    {}
+    <input
+    
+        type="text"
+        value={newEvent}
+        placeholder="Add new event" //empty placeholder
+    
+    />
+
+    {}
+    <button>Add new event</button>
+    
+
+    <h4 style={{ margin: 0, padding: 0 }}>Monday:</h4>
+    <h4 style={{ margin: 0, padding: 0 }}>Tuesday:</h4>
+    <h4 style={{ margin: 0, padding: 0 }}>Wednesday:</h4>
+    <h4 style={{ margin: 0, padding: 0 }}>Thursday:</h4>
+    <h4 style={{ margin: 0, padding: 0 }}>Friday:</h4>
+    
+    </div>
+  );
+}
+ 
+export default Eventsreact;
+
+//comment voor push ff
